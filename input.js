0b883cb5c6dfa17973f2df21dcbeaf244ef8b8f3
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-01 19:46:38 +0000

    Added wasd controls to user input handler. Working as expected

--- input.js
+++ input.js
@@ -1,10 +1,24 @@
+let connection;
+
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
+  } else {
+    if (data === 'w') {
+      connection.write('Move: up');
+    } else if (data === 'a') {
+      connection.write('Move: left');
+    } else if (data === 's') {
+      connection.write('Move: down');
+    } else if (data === 'd') {
+      connection.write('Move: right');
+    }
+    console.log(data);
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
